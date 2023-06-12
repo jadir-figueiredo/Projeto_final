dd3813021f03f9d993f13a4c6128c689e9fa3720
jadirf31 <jadirf31@gmail.com>
2023-06-13 00:51:19 +0100

    Projeto final

---
+++ Projeto_final/app.py
@@ -0,0 +1,166 @@
+# Encerra completamente a execução do programa
+import sys
+
+# Para importar uma biblioteca para trabalhar-mos fora do ambiente de código
+import PySimpleGUI as sg
+
+from datetime import datetime, timedelta
+
+# Importa a tabela de livros do módulo 'livros'
+from livros import tabela_livros
+
+# Importa as funções de acesso
+from funcoes import (
+    cadastrar_usario, validar_email,
+    verificar_usuario, verificar_usuario_cadastrado,
+    exibir_janela_cadastro
+    )
+
+# Importa a função de enviar email
+from funcoes import enviar_email
+
+
+data_atual = datetime.now()
+
+data_devolucao = data_atual + timedelta(days=7)
+
+data_devolucao_str = data_devolucao.strftime("%d/%m/%Y")
+
+# Define o tema visual da interface gráfica
+sg.theme("DarkTeal9")
+
+botoes = sg.Column([
+    [sg.Button("Entrar", size=(10, 1)),
+     sg.Button("Cadastrar", size=(10, 1)),
+     sg.Button("Devolução", size=(10, 1)),
+     sg.Button("Cancelar", size=(10, 1))]],
+     justification='center', )
+
+layout = [
+    [sg.Text("Nome Completo:", font=("SegoeUI", 12)),
+     sg.Input(key="-NOME-")],
+    [sg.Text("E-mail Válido:", font=("SegoeUI", 12)),
+     sg.Input(key="-EMAIL-")],
+    [botoes]
+]
+
+janela_cadastro = sg.Window("Cadastro Biblioteca Lisboa", layout,
+                            resizable=False, size=(500, 150), finalize=True)
+
+while True:
+    event, values = janela_cadastro.read()
+    if event in (sg.WIN_CLOSED, "Cancelar"):
+        sys.exit()
+    if event == "Cadastrar":
+        nome_cadastro = values["-NOME-"].strip()
+        email_cadastro = values["-EMAIL-"].strip()
+        if not nome_cadastro or not email_cadastro:
+            sg.popup("Preencha todos os campos!")
+        elif not validar_email(email_cadastro):
+            sg.popup("Email inválido!")
+        elif verificar_usuario_cadastrado(nome_cadastro, email_cadastro):
+            sg.popup("Usuário já cadastrado.")
+        else:
+            cadastrar_usario(nome_cadastro, email_cadastro)
+            sg.popup("Usuário cadastrado com sucesso!")
+    elif event == "Entrar":
+        nome_verificar = values["-NOME-"].strip()
+        email_verificar = values["-EMAIL-"].strip()
+        if not nome_verificar or not email_verificar:
+            sg.popup("Preencha todos os campos!")
+        elif verificar_usuario(nome_verificar, email_verificar):
+            janela_cadastro.close()
+            break
+        else:
+            sg.popup("Usuário não encontrado!")
+
+    janela_cadastro["-NOME-"].update("")
+    janela_cadastro["-EMAIL-"].update("")
+    janela_cadastro.bring_to_front()
+
+# Cria uma lista de listas com os dados dos livros da tabela
+dados_livros = [[livro["Título"], livro["Autor"], livro["Data"],
+                livro["Gênero"]] for livro in tabela_livros]
+
+# Ordena os dados dos livros em ordem alfabética de A a Z pelo título
+dados_livros = sorted(dados_livros, key=lambda x: x[0])  # type: ignore
+
+# Define os cabeçalhos das colunas da tabela
+header = ["Título", "Autor", "Data", "Gênero"]
+
+# Cria uma tabela com os valores e cabeçalhos especificados
+tabela = sg.Table(values=dados_livros, headings=header,
+                  justification="center", key="-TABLE-",
+                  select_mode=sg.TABLE_SELECT_MODE_BROWSE,
+                  enable_events=True, col_widths=[30, 20, 12, 15, 10],
+                  num_rows=len(dados_livros), bind_return_key=True,
+                  auto_size_columns=False,
+                  expand_x=True, expand_y=False)
+
+botoes = sg.Column([
+    [sg.Button("Escolher", size=(10, 1)),
+     sg.Button("Cancelar", size=(10, 1))]],
+     justification='center')
+
+# Adiciona um elemento para exibir a informação do livro a ser devolvido
+texto_devolucao = sg.Text("Livro a ser devolvido: ", font="SegoeUI")
+
+# Define o layout da janela com um texto, botões e a tabela
+layout = [
+    [sg.Text("Escolha seu livro", font="SegoeUI")],
+    [tabela], [botoes]
+]
+
+# Cria a janela com o título "Biblioteca Lisboa" e o layout especificado
+janela_escolha_livros = sg.Window("Biblioteca Lisboa", layout, resizable=False,
+                                  finalize=True)
+janela_escolha_livros.Maximize()
+
+# Indica que o livro foi devolvido inicialmente
+livro_devolvido = False
+
+while True:
+    # Lê os eventos e os valores dos elementos da janela
+    evento, valores = janela_escolha_livros.read()
+    if evento == sg.WINDOW_CLOSED or evento == "Cancelar":
+        break
+    elif evento == "Escolher":
+        if not livro_devolvido:
+            sg.popup("Você deve devolver o livro antes de escolher outro.")
+            continue
+        # Obtém as linhas selecionadas na tabela
+        linhas_selecionadas = tabela.SelectedRows
+        if len(linhas_selecionadas) == 0:
+            sg.popup("Escolha um livro", non_blocking=True)
+        else:
+            livro_escolhido = [dados_livros[indice]
+                               for indice in linhas_selecionadas]
+            titulo = livro_escolhido[0][0]
+            autor = livro_escolhido[0][1]
+            data = livro_escolhido[0][2]
+            genero = livro_escolhido[0][3]
+            mensagem = (f"Livro escolhido: "
+                        f"{titulo}, {autor}, {data}, {genero}\n"
+                        f"Prazo de devolucão são de 07 dias.")
+
+            texto_devolucao.update(f"Livro a ser devolvido: "
+                                   f"{titulo}, {autor}, {data}, {genero}")
+
+            result = sg.popup_ok_cancel(mensagem)
+
+            if result == "OK":
+                livro_devolvido = True
+                email_usuario = values["-EMAIL-"]
+                assunto_email = "Livro Escolhido"
+                corpo_email = (f"Você escolheu o livro: "
+                               f"{titulo}, {autor}, {data}, {genero}\n"
+                               f"A data de devolução é até o dia: "
+                               f"{data_devolucao_str}")
+                enviar_email(email_usuario, assunto_email, corpo_email)
+                sg.popup("Você recebeu um email confirmando sua escolha,\n"
+                         "juntamente com a data da devolução.\n"
+                         "BOA LEITURA!")
+                exibir_janela_cadastro()
+                break
+
+janela_escolha_livros.close()
---
+++ Projeto_final/funcoes.py
@@ -0,0 +1,145 @@
+import PySimpleGUI as sg
+import sys
+import re
+import pandas as pd
+import smtplib
+from smtplib import SMTPException
+
+
+def cadastrar_usario(nome, email):
+    # Cria um DataFrame com os dados do usuário
+    df_usuario = pd.DataFrame({"Nome": [nome.lower()],
+                               "Email": [email.lower()]})
+
+    # Carrega a planilha existente ou cria um novo arquivo
+    try:
+        df_planilha = pd.read_excel("usuarios.xlsx")
+        df_planilha = pd.concat([df_planilha, df_usuario], ignore_index=True)
+    except FileNotFoundError:
+        df_planilha = df_usuario
+
+    # Salva os dados na planilha
+    df_planilha.to_excel("usuarios.xlsx", index=False)
+
+
+def validar_email(email):
+    # Expressão regular para validar o formato do e-mail
+    padrao_email = r"[^@]+@[^@]+\.[^@]+"
+    return re.match(padrao_email, email) is not None
+
+
+def verificar_usuario(nome, email):
+    # Função para verificar cadastro
+    try:
+        # Carregar planilha
+        df_planilha = pd.read_excel("usuarios.xlsx")
+        # Verificar se o usuario está presente na planilha
+        usuario_encontrado = df_planilha[(df_planilha["Nome"] == nome.lower())
+                                         & (df_planilha["Email"] ==
+                                            email.lower())]
+        if not usuario_encontrado.empty:
+            return True
+    except FileNotFoundError:
+        pass
+
+    return False  # Se o usuário não for encontrado
+
+
+def verificar_usuario_cadastrado(nome, email):
+    try:
+        df_planilha = pd.read_excel("usuarios.xlsx")
+        usuario_encontrado = df_planilha[
+            (df_planilha["Nome"] == nome.lower()) &
+            (df_planilha["Email"] == email.lower())
+        ]
+        return not usuario_encontrado.empty
+    except FileNotFoundError:
+        return False
+
+
+# Função para enviar e-mails
+def enviar_email(destinatario, assunto, corpo):
+    # Configurações do remetente e servidor SMTP
+    remetente = "projetofinal.python@gmail.com"
+    senha = "ozcgrtuowtzcnuka"
+    servidor_smtp = "smtp.gmail.com"
+    porta_smtp = 587
+
+    # Constrói a mensagem de e-mail
+    mensagem = (f"Subject: {assunto}\n\n{corpo}")
+
+    try:
+        # Estabelece uma conexão com o servidor SMTP
+        with smtplib.SMTP(servidor_smtp, porta_smtp) as servidor:
+            # Inicia a criptografia da conexão
+            servidor.starttls()
+
+            # Realiza a autenticação no servidor
+            servidor.login(remetente, senha)
+
+            # Codifica a mensagem para utf-8
+            mensagem = mensagem.encode("utf-8")
+
+            # Envia o e-mail
+            servidor.sendmail(remetente, destinatario, mensagem)
+
+            # Exibe mensagem de sucesso
+            print("E-mail enviado com sucesso!")
+
+    except SMTPException as erro:
+        # Trata exceções que possam ocorrer durante o envio
+        print(f"Erro ao enviar e-mail: {erro}")
+
+
+def exibir_janela_cadastro():
+
+    botoes = sg.Column([
+        [sg.Button("Entrar", size=(10, 1)),
+         sg.Button("Cadastrar", size=(10, 1)),
+         sg.Button("Devolução", size=(10, 1)),
+         sg.Button("Cancelar", size=(10, 1))]],
+         justification='center', )
+
+    layout = [
+        [sg.Text("Nome Completo:", font=("SegoeUI", 12)),
+         sg.Input(key="-NOME-")],
+        [sg.Text("E-mail Válido:", font=("SegoeUI", 12)),
+         sg.Input(key="-EMAIL-")],
+        [botoes]
+    ]
+
+    janela_cadastro = sg.Window("Cadastro Biblioteca Lisboa", layout,
+                                resizable=False, size=(500, 150),
+                                finalize=True)
+
+    while True:
+        event, values = janela_cadastro.read()
+        if event in (sg.WIN_CLOSED, "Cancelar"):
+            sys.exit()
+        if event == "Cadastrar":
+            nome_cadastro = values["-NOME-"].strip()
+            email_cadastro = values["-EMAIL-"].strip()
+            if not nome_cadastro or not email_cadastro:
+                sg.popup("Preencha todos os campos!")
+            elif not validar_email(email_cadastro):
+                sg.popup("Email inválido!")
+            elif verificar_usuario_cadastrado(nome_cadastro, email_cadastro):
+                sg.popup("Usuário já cadastrado.")
+            else:
+                cadastrar_usario(nome_cadastro, email_cadastro)
+                sg.popup("Usuário cadastrado com sucesso!")
+        elif event == "Entrar":
+            nome_verificar = values["-NOME-"].strip()
+            email_verificar = values["-EMAIL-"].strip()
+            if not nome_verificar or not email_verificar:
+                sg.popup("Preencha todos os campos!")
+            elif verificar_usuario(nome_verificar, email_verificar):
+                janela_cadastro.close()
+                break
+            else:
+                sg.popup("Usuário não encontrado!")
+
+        janela_cadastro["-NOME-"].update("")
+        janela_cadastro["-EMAIL-"].update("")
+        janela_cadastro.finalize()  # Finaliza a janela para permitir operações
+        janela_cadastro.un_hide()  # Exibe a janela novamente
